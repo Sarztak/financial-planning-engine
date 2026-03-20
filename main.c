55b32d09de86a605265f7f7a5ffd6bc5a9d76aee
Sarztak <sar3142168@gmail.com>
2026-03-19 19:21:46 -0500

    added function to compute AGI; it is not around the corner, it is already here

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <math.h>
 #define GROWTH_RATE 0.07
 
+// this is a client; it has income expenses and retirement_age
 typedef struct {
     int age;
     double income;
@@ -11,11 +12,13 @@ typedef struct {
     double retirement_age;
 } Client;
 
+// this is account which has balance and a type IRA, Roth, 401K etc
 typedef struct {
     double balance;
     char type[20];
 } Account;
 
+// This is a state of cash at a particular year
 typedef struct {
     int age;
     double balance;
@@ -23,8 +26,11 @@ typedef struct {
     double expenses;
 } ProjectionState;
 
+// this is tax calcuation by brackets after standard deductions
 double calculate_tax(double income) {
-    double taxable = income - 14600; // income - standard deduction;
+    double std_deduction = 14600.0; // income - standard deduction;
+
+    double taxable = income - std_deduction; 
     if (taxable <= 0) 
         return 0;
     else if (taxable <= 11000)
@@ -39,6 +45,7 @@ double calculate_tax(double income) {
         return taxable * 0.37;
 }
 
+// function that computes a random value from normal distribution using Box-Muller transform given a mean and a stddev
 double normal_random(double mean, double stddev) {
     double u1 = ((double)rand() / RAND_MAX);
     if (u1 == 0) u1 = 0.0001; // log can become -inf if u1 is zero;
@@ -47,11 +54,7 @@ double normal_random(double mean, double stddev) {
     return mean + z * stddev;
 }
 
-double random_return() {
-    return normal_random(0.10, 0.15);
-}
-
-double gross_withdrawal(double expenses) {
+double 401k_gross_withdrawal(double expenses) {
     // here is what I can understand so my initial guess is the middle value 1. 5 expenses then I check how much I get after taxes. now if this amount is even less that what I want as my expenses then my guess is wrong, I can't really have the amount that I am withdrawing being less than what I want as expense; I want higher than that so my lower limit increases to mid and vice versa if my net is higher than expense then I am withdrawing more so I lower my search range. then at some point low and high converse close enough so that expenses = net and I return the mid point of low and high so the error in estimation can be at max 0.005 dollars
 
     double low = expenses;
@@ -66,12 +69,50 @@ double gross_withdrawal(double expenses) {
     return (low + high) / 2.0;
 }
 
+double get_pct_ss_taxable(double income) {
+    if (income < 25000)
+        return 0;
+    else if (income < 34000)
+        return 0.5;
+    else 
+        return 0.85;
+}
+
+double compute_agi(double net_income, double ss_benefit) {
+    double low = net_income;  // the lower limit is when tax on ss is zero 
+    double high = net_income + ss_benefit * 0.85; // the upper limit is when tax on ss is 85%
+    // the AGI cannot be out of this limit; these are upper and lower bounds
+
+    double err = 0.01; // the err is $0.01
+    while (high - low > err) {
+        double mid = (low + high) / 2.0; // mid is our initial guess
+
+        // using mid calculate combined income and use that to determine % of ss taxable
+        double combined_income = mid + (ss_benefit * 0.5);
+        double pct_ss_taxable = get_pct_ss_taxable(combined_income);
+
+        // not the right hand side of equation AGI = x + taxable_ss(AGI) can be solved by using mid as our guess and taxable_ss = ss_benefit * pct_ss_taxable
+        double rhs = net_income + ss_benefit * pct_ss_taxable;
+
+
+        // now my left hand side is mid and if my right hand side is less that lhs
+        // then I need to increase my search space
+        if (mid > rhs)
+            low = mid;
+        else
+            high = mid;
+    }
+
+    return (low + high) / 2.0;
+}
+
+// this function computes the income for the next year after all the deductions
 ProjectionState advance_one_year(ProjectionState current) {
     ProjectionState next = current;
     next.age += 1;
     double net_income = current.income - calculate_tax(current.income) - current.expenses;
     next.balance += net_income;
-    double rand_growth_rate = random_return();
+    double rand_growth_rate = normal_random(0.10, 0.15);
     next.balance *= (1 + rand_growth_rate);
     return next;
 }
@@ -96,12 +137,16 @@ int main(int argc, char *argv[]) {
         int final_age = 90; 
 
         for (int i = state.age; i <= final_age; i++) {
+            if (i >= retirement_age) state.income = 0; // income zero at retirement
+            if (state.age >= 67) state.income += 42000; // income from ss 
+
+            // the logic is income after retirement is zero but then you have
+            // income from the social security.
             if (i >= retirement_age) {
-                state.income = 0; // income stops after retirement age
-                state.balance -= gross_withdrawal(state.expenses);
+                state.balance -= 401k_gross_withdrawal(state.expenses);
                 // in retirement we withdraw money which is taxable 
             }
-            state = advance_one_year(state);
+            state = advance_one_year(state); // this computes the taxes 
 
             // printf("Balance: $%.2f\n", state.balance);
             if (state.balance <= 0) {
